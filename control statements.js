7f2284e3071d94a3ab9a69eeb682acf950ba8a32
Sahil Garg <sahilgarg2801@gmail.com>
2024-02-06 16:58:40 +0530

    Control Flow Statement (If_______Else)

---
+++ control statements.js
@@ -0,0 +1,10 @@
+// if____Else
+// if raining=raincoat
+// else no raining
+
+var tomr='rain';
+if(tomr=='rain'){
+console.log('take a raincoat');
+}else{
+    console.log('no need to take raincoat');
+} 
